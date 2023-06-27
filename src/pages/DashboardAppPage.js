1b5d9935d69f68329c5be51948736dd9d97df231
kosta39 <kokson65@gmail.com>
2023-06-28 00:09:23 +0200

    added usage of tracing public api to display user ip address and location

--- src/pages/DashboardAppPage.js
+++ src/pages/DashboardAppPage.js
@@ -2,7 +2,7 @@ import { Helmet } from 'react-helmet-async';
 import { faker } from '@faker-js/faker';
 // @mui
 import { useTheme } from '@mui/material/styles';
-import { Grid, Container, Typography } from '@mui/material';
+import {Grid, Container, Typography, Box} from '@mui/material';
 // components
 import Iconify from '../components/iconify';
 // sections
@@ -17,11 +17,19 @@ import {
   AppCurrentSubject,
   AppConversionRates,
 } from '../sections/@dashboard/app';
+import {useEffect, useState} from "react";
 
 // ----------------------------------------------------------------------
 
 export default function DashboardAppPage() {
-  const theme = useTheme();
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+      getCloudflareJSON().then((data) => {
+          console.log(data);
+          setData(data);
+      })
+  }, [])
 
   return (
     <>
@@ -30,7 +38,17 @@ export default function DashboardAppPage() {
       </Helmet>
 
       <Container maxWidth="xl">
+          <Box>
+              <Typography variant="body1">IP address: {data?.ip}</Typography>
+              <Typography variant="body1">Location: {data?.loc}</Typography>
+          </Box>
       </Container>
     </>
   );
+}
+
+async function getCloudflareJSON() {
+    let data = await fetch('https://1.1.1.1/cdn-cgi/trace').then(res=>res.text())
+    let arr = data.trim().split('\n').map(e=>e.split('='))
+    return Object.fromEntries(arr)
 }
